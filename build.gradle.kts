12c6932d0d5184425cbb4490bda741dba1723ef4
marzelwidmer <marzelwidmer@gmail.com>
2021-03-02 21:25:34 +0100

    Initial Commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,22 @@
 import org.jetbrains.kotlin.gradle.dsl.Coroutines
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val logback_version: String by project
-val ktor_version: String by project
-val kotlin_version: String by project
-
 plugins {
     application
     kotlin("jvm") version "1.4.30"
     id("com.google.cloud.tools.jib") version "2.8.0"
 
 }
-
 group = "ch.keepcalm.demo"
 version = "0.0.1-SNAPSHOT"
 
-val mainClass by extra("io.ktor.server.netty.EngineMain")
+val logback_version: String by project
+val ktor_version: String by project
+val kotlin_version: String by project
+val main_class by extra("io.ktor.server.netty.EngineMain")
 
 application {
-    mainClassName = mainClass.toString()
+    mainClassName = main_class
 
     applicationDefaultJvmArgs = listOf(
         "-server",
@@ -30,21 +28,7 @@ application {
     )
 }
 
-jib {
-    from {
-        image = "registry.access.redhat.com/ubi8/openjdk-11:latest"
-    }
-    container {
-        ports = listOf("8080")
-        mainClass = this@Build_gradle.mainClass
-    }
-}
-
-repositories {
-    mavenLocal()
-    jcenter()
-    maven { url = uri("https://kotlin.bintray.com/ktor") }
-}
+java.sourceCompatibility = JavaVersion.VERSION_11
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlin_version")
@@ -55,11 +39,44 @@ dependencies {
     implementation("io.ktor:ktor-auth-jwt:$ktor_version")
     implementation("io.ktor:ktor-locations:$ktor_version")
     implementation("io.ktor:ktor-metrics:$ktor_version")
+    implementation("io.ktor:ktor-html-builder:$ktor_version")
+
     testImplementation("io.ktor:ktor-server-tests:$ktor_version")
 }
+
+
+
 
 kotlin.sourceSets["main"].kotlin.srcDirs("src")
 kotlin.sourceSets["test"].kotlin.srcDirs("test")
 
 sourceSets["main"].resources.srcDirs("resources")
 sourceSets["test"].resources.srcDirs("testresources")
+
+jib {
+    from {
+        image = "registry.access.redhat.com/ubi8/openjdk-11:latest"
+    }
+
+    container {
+        ports = listOf("8080")
+        mainClass = main_class
+
+        // good defauls intended for Java 8 (>= 8u191) containers
+        jvmFlags = listOf(
+            "-server",
+            "-Djava.awt.headless=true",
+            "-XX:InitialRAMFraction=2",
+            "-XX:MinRAMFraction=2",
+            "-XX:MaxRAMFraction=2",
+            "-XX:+UseG1GC",
+            "-XX:MaxGCPauseMillis=100",
+            "-XX:+UseStringDeduplication"
+        )
+    }
+}
+
+repositories {
+    jcenter()
+    mavenCentral()
+}
